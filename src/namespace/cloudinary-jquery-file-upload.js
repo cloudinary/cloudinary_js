87d7a6a4d911b06abf7d44c27280d652ef8e7942
Amir Tocker <amir.tocker@cloudinary.com>
2019-01-29 19:03:03 +0200

    Fix ES6 modules

--- src/namespace/cloudinary-jquery-file-upload.js
+++ src/namespace/cloudinary-jquery-file-upload.js
@@ -1,72 +1,60 @@
   /**
    * Creates the namespace for Cloudinary
    */
-export {
-  default as utf8_encode
-} from '../utf8_encode';
-
-export {
-  default as crc32
-} from '../crc32';
-
+import utf8_encode from '../utf8_encode';
+import crc32 from '../crc32';
 import * as Util from '../util';
+import Transformation from '../transformation';
+import Condition from '../condition';
+import Configuration from '../configuration';
+import HtmlTag from '../tags/htmltag';
+import ImageTag from '../tags/imagetag';
+import PictureTag from '../tags/picturetag';
+import VideoTag from '../tags/videotag';
+import ClientHintsMetaTag from '../tags/clienthintsmetatag';
+import Layer from '../layer/layer';
+import FetchLayer from '../layer/fetchlayer';
+import TextLayer from '../layer/textlayer';
+import SubtitlesLayer from '../layer/subtitleslayer';
+import Cloudinary from '../cloudinary';
+import CloudinaryJQuery from '../jquery-file-upload';
 
-export {
-  Util
+export default {
+  ClientHintsMetaTag,
+  Cloudinary,
+  CloudinaryJQuery,
+  Condition,
+  Configuration,
+  crc32,
+  FetchLayer,
+  HtmlTag,
+  ImageTag,
+  Layer,
+  PictureTag,
+  SubtitlesLayer,
+  TextLayer,
+  Transformation,
+  utf8_encode,
+  Util,
+  VideoTag
 };
 
 export {
-  default as Transformation
-} from '../transformation';
-
-export {
-  default as Condition
-} from '../condition';
-
-export {
-  default as Configuration
-} from '../configuration';
-
-export {
-  default as HtmlTag
-} from '../tags/htmltag';
-
-export {
-  default as ImageTag
-} from '../tags/imagetag';
-
-export {
-  default as PictureTag
-} from '../tags/picturetag';
-
-export {
-  default as VideoTag
-} from '../tags/videotag';
-
-export {
-  default as ClientHintsMetaTag
-} from '../tags/clienthintsmetatag';
-
-export {
-  default as Layer
-} from '../layer/layer';
-
-export {
-  default as FetchLayer
-} from '../layer/fetchlayer';
-
-export {
-  default as TextLayer
-} from '../layer/textlayer';
-
-export {
-  default as SubtitlesLayer
-} from '../layer/subtitleslayer';
-
-export {
-  default as Cloudinary
-} from '../cloudinary';
-
-export {
-  default as CloudinaryJQuery
-} from '../jquery-file-upload';
+  ClientHintsMetaTag,
+  Cloudinary,
+  CloudinaryJQuery,
+  Condition,
+  Configuration,
+  crc32,
+  FetchLayer,
+  HtmlTag,
+  ImageTag,
+  Layer,
+  PictureTag,
+  SubtitlesLayer,
+  TextLayer,
+  Transformation,
+  utf8_encode,
+  Util,
+  VideoTag
+};
