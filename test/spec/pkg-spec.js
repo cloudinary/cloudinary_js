766cea3f2c07444f3b7511f91ffa96b23425dd0b
maoznir <nir.maoz@cloudinary.com>
2019-08-12 13:20:46 +0300

    Update packages test according to pr comments

--- test/spec/pkg-spec.js
+++ test/spec/pkg-spec.js
@@ -15,22 +15,81 @@ jasmine.getEnv().addReporter(new SpecReporter({  // add jasmine-spec-reporter
   },
 }));
 
+//List of required packages and files
+const packages = [
+  {
+    name: 'cloudinary-core',
+    files: [
+      'src',
+      'cloudinary-core.d.ts',
+      'cloudinary-core.js',
+      'cloudinary-core.min.js',
+      'cloudinary-core.js.map',
+      'cloudinary-core-shrinkwrap.js',
+      'cloudinary-core-shrinkwrap.js.map',
+      'cloudinary-core-shrinkwrap.min.js',
+      'package.json',
+      'README.md'
+    ]
+  },
+  {
+    name: 'cloudinary-jquery',
+    files: [
+      'src',
+      'cloudinary-jquery.d.ts',
+      'cloudinary-jquery.js',
+      'cloudinary-jquery.js.map',
+      'cloudinary-jquery.min.js',
+      'package.json',
+      'README.md'
+    ]
+  },
+  {
+    name: 'cloudinary-jquery-file-upload',
+    files: [
+      'src',
+      'cloudinary-jquery-file-upload.d.ts',
+      'cloudinary-jquery-file-upload.js',
+      'cloudinary-jquery-file-upload.js.map',
+      'cloudinary-jquery-file-upload.min.js',
+      'package.json',
+      'README.md'
+    ]
+  }
+];
+
 /**
  * Get path of given fileName in given pkgName
  * @param pkgName
  * @param fileName
  * @returns {*}
  */
-const getPath = (pkgName, fileName) => path.join(__dirname, '..', '..', 'pkg', pkgName, fileName);
+const getPath = (pkgName, fileName) => {
+  if (fileName) {
+    return path.join(__dirname, '..', '..', 'pkg', pkgName, fileName);
+  }
 
+  return path.join(__dirname, '..', '..', 'pkg', pkgName);
+};
 
 /**
- * Test that a given fileName exist in given pkgName
- * @param pkgName
- * @param fileName
+ * Return an array of files in given folder path
+ * @param folderPath
+ * @returns {Array}
  */
-testFileExists = (pkgName, fileName) => {
-  const filePath = getPath(pkgName, fileName);
+const getFilesList = (folderPath) => {
+  let fileList = [];
+  fs.readdirSync(folderPath).forEach(file => {
+    fileList.push(file);
+  });
+  return fileList;
+};
+
+/**
+ * Assert that a given path exists
+ * @param filePath
+ */
+assertPathExists = (filePath) => {
   const result = fs.existsSync(filePath);
   it(filePath, () => {
     expect(result).toEqual(true);
@@ -38,51 +97,36 @@ testFileExists = (pkgName, fileName) => {
 };
 
 /**
- * Test that all given files exist in given pkgName
- * @param pkgName
- * @param files
+ * Verify that a package has only required files
+ * @param pkg
  */
-testPkg = (pkgName, files) => {
-  describe(`${pkgName}:`, function () {
-    files.forEach(fileName => testFileExists(pkgName, fileName));
+verifyPackageConsistency = (pkg) => {
+  const currentFiles = getFilesList(getPath(pkg.name));
+  const unnecessaryFiles = currentFiles.filter(file => pkg.files.indexOf(file) < 0);
+
+  describe(`${pkg.name}:`, function () {
+
+    //test that necessary files exist
+    describe(`Required Files:`, function () {
+      pkg.files.forEach(fileName => {
+        it(fileName, () => {
+          expect(currentFiles.includes(fileName)).toBeTruthy();
+        });
+      });
+    });
+
+    //test that unnecessary files do not exist
+      describe(`Unnecessary Files:`, function () {
+        unnecessaryFiles.forEach(fileName => {
+          it(fileName, () => {
+            expect(fileName).toBeUndefined();
+          });
+        });
+      });
   });
 };
 
-testPkg('cloudinary-core',
-  [
-    'src',
-    'cloudinary-core.d.ts',
-    'cloudinary-core.js',
-    'cloudinary-core.min.js',
-    'cloudinary-core.js.map',
-    'cloudinary-core-shrinkwrap.js',
-    'cloudinary-core-shrinkwrap.js.map',
-    'cloudinary-core-shrinkwrap.min.js',
-    'package.json',
-    'README.md'
-  ]
-);
-
-testPkg('cloudinary-jquery',
-  [
-    'src',
-    'cloudinary-jquery.d.ts',
-    'cloudinary-jquery.js',
-    'cloudinary-jquery.js.map',
-    'cloudinary-jquery.min.js',
-    'package.json',
-    'README.md'
-  ]
-);
-
-testPkg('cloudinary-jquery-file-upload',
-  [
-    'src',
-    'cloudinary-jquery-file-upload.d.ts',
-    'cloudinary-jquery-file-upload.js',
-    'cloudinary-jquery-file-upload.js.map',
-    'cloudinary-jquery-file-upload.min.js',
-    'package.json',
-    'README.md'
-  ]
-);
+//Verify each package from packages list
+packages.forEach(pkg => {
+  verifyPackageConsistency(pkg);
+});
