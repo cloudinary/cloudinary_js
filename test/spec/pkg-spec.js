d8a6d631e55287aa01197276c339592eb6c7ce89
maoznir <nir.maoz@cloudinary.com>
2019-08-25 15:16:26 +0300

    Add build files test

--- test/spec/pkg-spec.js
+++ test/spec/pkg-spec.js
@@ -20,6 +20,7 @@ jasmine.getEnv().addReporter(new SpecReporter({
 
 const mainPath = path.join(__dirname, '..', '..');
 const pkgPath = path.join(__dirname, '..', '..', 'pkg');
+const buildPath = path.join(__dirname, '..', '..', 'dist');
 const version = getVersion();
 const commonFiles = ['src', 'package.json', 'README.md'];
 const commonExtensions = {
@@ -69,6 +70,9 @@ function verifyPackageConsistency(pkg) {
     });
     it(`Package version should be same as cloudinary_js: ${version}`, () => {
       expect(pkgVersion).toEqual(version);
+    });
+    it(`Package files should equal files in dist folder`, () => {
+      expect(getUnequalBuildFiles(pkg, actualFiles)).toEqual([]);
     });
   });
 }
@@ -166,6 +170,42 @@ function getVersion(pkg = null) {
   }
   // get pkg version
   return JSON.parse(fs.readFileSync(`${pkgPath}/${pkg.name}/package.json`)).version;
+}
+
+/**
+ * Get list of package files that are not equal build files
+ * @param pkg
+ * @param pkgFiles
+ * @returns Array of file names
+ */
+function getUnequalBuildFiles(pkg, pkgFiles) {
+  let result = [];
+
+  //filter files that should not exist in build but should exist in package
+  const files = pkgFiles.filter(file => !['README.md', `${pkg.name}.d.ts`, 'package.json', 'src'].includes(file));
+
+  files.forEach(fileName => {
+    const pkgFilePath = `${pkgPath}/${pkg.name}/${fileName}`;
+    const buildFilePath = `${buildPath}/${fileName}`;
+    const buildFileExists = fs.existsSync(buildFilePath);
+    const buildFileEquals = buildFileExists && compareFiles(buildFilePath, pkgFilePath);
+    if (!buildFileEquals) {
+      result.push(fileName);
+    }
+  });
+  return result;
+}
+
+/**
+ * Compare two files content
+ * @param fileA
+ * @param fileB
+ * @returns true if equal, false otherwise
+ */
+function compareFiles(fileA, fileB) {
+  const bufferA = fs.readFileSync(fileA);
+  const bufferB = fs.readFileSync(fileB);
+  return bufferA.equals(bufferB);
 }
 
 requiredPackages.forEach(pkg => {
