b780cc00515140322b5e0585abd94c25c0d24203
Patrick Tolosa <patrick.tolosa@cloudinary.com>
2020-01-22 11:06:01 +0200

    Removed /.bin/karma from the pathing of a script

--- tools/scripts/test.pkgs.minified.sh
+++ tools/scripts/test.pkgs.minified.sh
@@ -1,1 +1,1 @@
-for pkg in core jquery jquery-file-upload; do node_modules/.bin/karma start --single-run --cloudinary.minified --cloudinary.pkg=${pkg}; done
+for pkg in core jquery jquery-file-upload; do karma start --single-run --cloudinary.minified --cloudinary.pkg=${pkg}; done
