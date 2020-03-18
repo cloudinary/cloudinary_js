c43091e73ab1f841c43f0cb824a39a19f540bf1b
Patrick Tolosa <patrick.tolosa@cloudinary.com>
2020-03-18 13:09:30 +0200

    Add config file

---
+++ bundlewatch.config.js
@@ -0,0 +1,18 @@
+const bundlewatchConfig = {
+  files: [
+    {
+      path: './dist/cloudinary-core.min.js',
+      maxSize: '80kb',
+      compression: 'none',
+    },
+    {
+      path: './dist/cloudinary-core-shrinkwrap.min.js',
+      maxSize: '130kb',
+      compression: 'none',
+    },
+  ],
+
+  defaultCompression: 'gzip', // Do we want the results in gzip form?
+};
+
+module.exports = bundlewatchConfig;
