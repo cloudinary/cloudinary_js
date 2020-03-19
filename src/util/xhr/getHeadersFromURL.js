b4c5b46cb8be7907b2fcb745d8ffdb1e91dbcb9b
Patrick Tolosa <patrick.tolosa@cloudinary.com>
2020-03-19 13:28:22 +0200

    Refactor getHeadersFromURL into smaller functions

--- src/util/xhr/getHeadersFromURL.js
+++ src/util/xhr/getHeadersFromURL.js
@@ -11,26 +11,15 @@ function getHeadersFromURL(url, maxTimeout) {
   return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
 
+    let timeoutID = setErrorHandling(xhr, maxTimeout, reject);
+
     xhr.onreadystatechange = function () {
       if (this.readyState === this.HEADERS_RECEIVED) {
         // clear the time-out rejection
-        clearTimeout(timerID); // clear timeout reject error
+        clearTimeout(timeoutID); // clear timeout reject error
         // Get the raw header string
         let headers = xhr.getAllResponseHeaders();
-
-        // Convert the header string into an array
-        // of individual headers
-        let arr = headers.trim().split(/[\r\n]+/);
-
-
-        // Create a map of header names to values
-        let headerMap = {};
-        arr.forEach(function (line) {
-          let parts = line.split(': ');
-          let header = parts.shift();
-          let value = parts.join(': ');
-          headerMap[header] = value;
-        });
+        let headerMap = headerStringToMap(headers);
 
         resolve({
           status: 'success',
@@ -39,23 +28,57 @@ function getHeadersFromURL(url, maxTimeout) {
       }
     };
 
-    let timerID = setTimeout(() => {
-      reject({
-        status: 'error',
-        message: 'Timeout reading headers from server'
-      });
-    }, maxTimeout);
-
-    xhr.onerror = function () {
-      clearTimeout(timerID); // clear timeout reject error
-      reject({
-        status: 'error',
-        message: 'Error reading headers from server'
-      });
-    };
     xhr.open('HEAD', url, true);
     xhr.send();
   });
+}
+
+/**
+ * Convert the Header string to a map of key:value
+ * @param {string} headers
+ * @return {{}}
+ */
+function headerStringToMap(headers) {
+  // Convert the header string into an array
+  // of individual headers
+  let arr = headers.trim().split(/[\r\n]+/);
+
+  // Create a map of header names to values
+  let headerMap = {};
+  arr.forEach(function (line) {
+    let parts = line.split(': ');
+    let header = parts.shift();
+    let value = parts.join(': ');
+    headerMap[header] = value;
+  });
+
+  return headerMap;
+}
+
+/**
+ *
+ * @param {XMLHttpRequest} xhr
+ * @param {number} maxTimeout
+ * @param {Function} rejectCb
+ * @return {number};
+ */
+function setErrorHandling(xhr, maxTimeout, rejectCb) {
+  let timeoutID = setTimeout(() => {
+    rejectCb({
+      status: 'error',
+      message: 'Timeout reading headers from server'
+    });
+  }, maxTimeout);
+
+  xhr.onerror = function () {
+    clearTimeout(timeoutID); // clear timeout reject error
+    rejectCb({
+      status: 'error',
+      message: 'Error reading headers from server'
+    });
+  };
+
+  return timeoutID;
 }
 
 
