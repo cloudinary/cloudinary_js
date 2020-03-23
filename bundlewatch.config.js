9e0b40be91fac7d7ce6277cb34588791df824912
Patrick Tolosa <patrick.tolosa@cloudinary.com>
2020-03-23 15:20:34 +0200

    Add bundlewatch Service

---
+++ bundlewatch.config.js
@@ -0,0 +1,23 @@
+const bundlewatchConfig = {
+  files: [
+    {
+      path: './dist/cloudinary-core.min.js',
+      maxSize: '20kb'
+    },
+    {
+      path: './dist/cloudinary-core-shrinkwrap.min.js',
+      maxSize: '27kb'
+    },
+    {
+      path: './dist/cloudinary-jquery.js',
+      maxSize: '45kb'
+    },
+    {
+      path: './dist/cloudinary-jquery-file-upload.js',
+      maxSize: '47kb'
+    }
+  ],
+  defaultCompression: 'gzip',  // Do we want the results in gzip form?
+};
+
+module.exports = bundlewatchConfig;
