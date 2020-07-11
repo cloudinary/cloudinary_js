bdc7867eb1348b8c1b7f425373772485f52316ef
maoznir <nir.maoz@cloudinary.com>
2020-07-11 12:04:10 +0300

    Fix karma config

--- tools/scripts/test.pkgs.sh
+++ tools/scripts/test.pkgs.sh
@@ -1,1 +1,1 @@
-for pkg in core jquery jquery-file-upload; do karma start --single-run --cloudinary.pkg=${pkg}; done
+for pkg in core jquery jquery-file-upload; do karma start --single-run --browsers=ChromeHeadless --cloudinary.pkg=${pkg}; done
