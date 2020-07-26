1ee5c27cfccc4d984bd0c71ca21288e46947d249
Nir Maoz <47939921+maoznir@users.noreply.github.com>
2020-07-26 10:12:08 +0300

    Fix lazy loading of responsive images (#253)

--- test/spec/automatic/lazy-load-spec.js
+++ test/spec/automatic/lazy-load-spec.js
@@ -1,17 +1,10 @@
 describe("Lazy Loaded Image", function() {
-  it("should return empty src on Firefox", function() {
+  it("should return empty src", function() {
     cl = cloudinary.Cloudinary.new({cloud_name: 'sdk-test'});
-    var img;
-    let options = {loading: "lazy"}
-    img = cl.image("sample", options);
+    let options = {loading: "lazy"};
+    let img = cl.image("sample", options);
     img.removeAttribute('src');
     cl.cloudinary_update(img, options);
-
-    if(navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-      expect(img).not.toContain('src');
-    }
-    if(navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
-      expect(img.getAttribute('src')).toEqual('http://res.cloudinary.com/sdk-test/image/upload/sample');
-    }
+    expect(img).not.toContain('src');
   });
 });
