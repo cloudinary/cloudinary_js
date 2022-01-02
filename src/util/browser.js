93a16fdaaaf337561d5f8de0621fc61e7124771e
echatzief <echatzief97@gmail.com>
2022-01-02 19:14:55 +0200

    Update function name

--- src/util/browser.js
+++ src/util/browser.js
@@ -7,7 +7,7 @@
 * Retrieve from the navigator the user agent property.
 * @returns user agent property.
 */
-function getNavigator(){
+function getUserAgent(){
   return navigator && navigator.userAgent || '';
 }
 
@@ -16,7 +16,7 @@ function getNavigator(){
  * @returns true if current browser is Android, false otherwise.
  */
 export function isAndroid(){
-  const userAgent = getNavigator();
+  const userAgent = getUserAgent();
   return (/Android/i).test(userAgent);
 }
 
@@ -25,7 +25,7 @@ export function isAndroid(){
  * @returns true if current browser is Edge, false otherwise.
  */
 export function isEdge(){
-  const userAgent = getNavigator();
+  const userAgent = getUserAgent();
   return (/Edg/i).test(userAgent);
 }
 
@@ -34,7 +34,7 @@ export function isEdge(){
  * @returns true if current browser is Chrome, false otherwise.
  */
 export function isChrome(){
-  const userAgent = getNavigator();
+  const userAgent = getUserAgent();
   return !isEdge() && ((/Chrome/i).test(userAgent) || (/CriOS/i).test(userAgent));
 }
 
@@ -46,6 +46,6 @@ export function isSafari(){
   // User agents for other browsers might include "Safari" so we must exclude them.
   // For example - this is the chrome user agent on windows 10:
   // Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36
-  const userAgent = getNavigator();
+  const userAgent = getUserAgent();
   return (/Safari/i).test(userAgent) && !isChrome() && !isAndroid() && !isEdge();
 }
