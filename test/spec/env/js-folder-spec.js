c50f5dc94bccc7e68bd84523166e8bfd14bd3046
Nir Maoz <47939921+maoznir@users.noreply.github.com>
2022-01-13 17:23:49 +0200

    Remove links to nonexistent mapfiles in js folder (#283)

--- test/spec/env/js-folder-spec.js
+++ test/spec/env/js-folder-spec.js
@@ -15,7 +15,7 @@ jasmine.getEnv().addReporter(new SpecReporter({
 
 // List of files to test
 const files = [
-  {name: 'canvas-to-blob.min.js', checkSum: "33ecbe6885ad339c3a5af0cfa063810d"},
+  {name: 'canvas-to-blob.min.js', checkSum: "7c7becb6f9ecf2defa663e70a6b3a0f5"},
   {name: 'jquery.cloudinary.js', checkSum: "388dffde6d46b70fae4f0dfc254f48c5"},
   {name: 'jquery.fileupload.js', checkSum: "4bfd85460689a29e314ddfad50c184e0"},
   {name: 'jquery.fileupload-image.js', checkSum: "7c40367b00f74b0c7c43bff009dde942"},
@@ -23,7 +23,7 @@ const files = [
   {name: 'jquery.fileupload-validate.js', checkSum: "a144e6149c89ed27e0b2d7fcfef09101"},
   {name: 'jquery.iframe-transport.js', checkSum: "f371e8d9f57329f90114d7b52dd5c7a4"},
   {name: 'jquery.ui.widget.js', checkSum: "3d0f0f5ca5d86c5a4b4fc33cda374a17"},
-  {name: 'load-image.all.min.js', checkSum: "c4cdadc311ce87bb6f89491d1289b0a3"}
+  {name: 'load-image.all.min.js', checkSum: "d0068a911289a0176efa4f5880987f1e"}
 ];
 
 /**
